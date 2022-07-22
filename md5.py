fc10f6b2061ec981ef33a2791c303df34f1fbe6e
Richard Antal Nagy <nagy.richard.antal@gmail.com>
2022-07-22 23:06:50 +0200

    Added module comment and explicit types to md5

--- md5.py
+++ md5.py
@@ -1,6 +1,15 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+'''
+Implementation of the md5 hash algorithm in python.
+This library is for learning purposes, so built-in functions should be preferred.
+https://github.com/dubniczky/Cryptography-Algorithms
+'''
+
 
 import binascii
+from typing import Callable
+
 
 start_chunks = {
     'A': 0x67452301,
@@ -10,18 +19,18 @@ start_chunks = {
 }
 
 SV = [
-    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 
+    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf,
     0x4787c62a, 0xa8304613, 0xfd469501, 0x698098d8, 0x8b44f7af,
-    0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 
-    0x49b40821, 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 
-    0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8, 0x21e1cde6, 
-    0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8, 
-    0x676f02d9, 0x8d2a4c8a, 0xfffa3942, 0x8771f681, 0x6d9d6122, 
-    0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70, 
+    0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e,
+    0x49b40821, 0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+    0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8, 0x21e1cde6,
+    0xc33707d6, 0xf4d50d87, 0x455a14ed, 0xa9e3e905, 0xfcefa3f8,
+    0x676f02d9, 0x8d2a4c8a, 0xfffa3942, 0x8771f681, 0x6d9d6122,
+    0xfde5380c, 0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
     0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05, 0xd9d4d039,
     0xe6db99e5, 0x1fa27cf8, 0xc4ac5665, 0xf4292244, 0x432aff97,
-    0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 
-    0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 
+    0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d,
+    0x85845dd1, 0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
     0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
 ]
 
@@ -33,7 +42,7 @@ def bit_rotate_left(target: int, shift: int) -> int:
     return upper | (target >> (32 - shift))
 
 
-def block_to_chunks(block: bytes, chunk_size):
+def block_to_chunks(block: bytes, chunk_size: int) -> list[int]:
     chunk_count = len(block) // chunk_size
     chunks = []
     for i in range(chunk_size):
@@ -59,18 +68,18 @@ def I(X: int, Y: int, Z: int) -> int:
     return Y ^ (X | (~Z))
 
 
-def rot_func(func, a,b,c,d,M,s,t):
+def rot_func(func: Callable[[int,int,int], int], a: int, b: int, c: int, d: int, M: int, s: int, t: int):
     return b + bit_rotate_left( (a + func(b,c,d) + M + t) , s)
 
 
-def format_int32(num):
+def format_int32(num: int) -> str:
     bighex = "{0:08x}".format(num)
     binver = binascii.unhexlify(bighex)
     return "{0:08x}".format(int.from_bytes(binver, byteorder='little'))
 
 
-def bit_count(bytes):
-    return len(bytes) * 8
+def bit_count(b: bytes) -> int:
+    return len(b) * 8
 
 
 def md5(data: str|bytes) -> str:
@@ -85,13 +94,13 @@ def md5(data: str|bytes) -> str:
     data = data + b'\x00' * pad_zero_length + length.to_bytes(8, byteorder='little')
     length = bit_count(data)
     iterations = length // 512
-    
+
     # Init start values
     A = start_chunks['A']
     B = start_chunks['B']
     C = start_chunks['C']
     D = start_chunks['D']
-    
+
     # Calculate rounds
     for i in range(iterations):
         a = A
@@ -180,7 +189,7 @@ def md5(data: str|bytes) -> str:
         d = rot_func(I, d,a,b,c, M[11], 10, SV[61] )
         c = rot_func(I, c,d,a,b, M[2], 15, SV[62] )
         b = rot_func(I, b,c,d,a, M[9], 21, SV[63] )
-        
+
         A = (A + a) % (2**32)
         B = (B + b) % (2**32)
         C = (C + c) % (2**32)
